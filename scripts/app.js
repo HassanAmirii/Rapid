135ad6032cf92bc1877d8d5a581e9784684a29fe
HassanAmirii <hassanamiri.ai@gmail.com>
2025-05-07 19:04:35 +0100

    testing mvp stg 1

---
+++ scripts/app.js
@@ -0,0 +1,79 @@
+document.getElementById("start").addEventListener("click", function () {
+  let app = document.getElementById("app");
+  let classSelected;
+  let subjectSelected;
+  let timeSelected;
+
+  function pickClass() {
+    app.innerHTML = `
+
+    <h1>Please select your current grade below</h1>
+
+    <div id="parent-Class">
+    
+      <button class="class-child" data-class="grade 1">grade 1</button>
+      <button class="class-child" data-class="grade 2">grade 2</button>
+      <button class="class-child" data-class="grade 3">grade 3</button>
+      <button class="class-child" data-class="grade 4">grade 4</button>
+    </div>
+
+`;
+  }
+  pickClass();
+  app.addEventListener("click", function (e) {
+    if (e.target.tagName === "BUTTON") {
+      if (e.target.dataset.class) {
+        classSelected = e.target.dataset.class;
+        pickSubject();
+      } else if (e.target.dataset.subject) {
+        subjectSelected = e.target.dataset.subject;
+        time();
+      } else if (e.target.dataset.time) {
+        timeSelected = e.target.dataset.time;
+        summary();
+      }
+    }
+  });
+
+  function pickSubject() {
+    app.innerHTML = `
+    <p>you just picked <strong>${classSelected}</strong> awesome, kindly pick a subject to continue</p>
+ <div id="parent-subject">
+  <button class="subject-child" data-subject="Maths">Maths</button>
+  <button class="subject-child" data-subject="English">English</button>
+  <button class="subject-child" data-subject="science">science</button>
+  <button class="subject-child" data-subject="civic">civic</button>
+</div> `;
+  }
+
+  function time() {
+    app.innerHTML = `
+    <p>you just picked <strong>${subjectSelected}</strong> awesome, kindly pick a time type to continue</p>
+ <div id="parent-time">
+  <button class="time-child" data-time="20minutes">20minutes - quiz </button>
+  <button class="time-child" data-time="40minutes">40minutes - exam </button>
+</div> `;
+  }
+
+  function summary() {
+    app.innerHTML = ` <h1>almost done, just a quick confirmation</h1>
+<p><strong> summary:</strong></p>
+<p>class picked: <strong>${classSelected}</strong> </p>
+<p>subject picked: <strong>${subjectSelected}</strong> </p>
+<p>time based picked: <strong>${timeSelected}</strong> </p>
+
+<p>make sure to be in a quiet place to avoid distractions</p>
+<p>have a jotter and a pencil close by for quick jotting or calculations </p>
+<p>good luck champ</p>
+
+<button id="beginSession"> start ${timeSelected} session</button>
+
+
+`;
+    document
+      .getElementById("beginSession")
+      .addEventListener("click", function () {
+        alert("${timeSelected} begins...");
+      });
+  }
+});
